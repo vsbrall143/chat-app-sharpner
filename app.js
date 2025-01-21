0f7caf198cb0d1d9996b4d1d82811d1b5f2efa1f
vsbrall143 <vsbrall143@gmail.com>
2025-01-21 18:34:37 +0530

    deploy 14

--- app.js
+++ app.js
@@ -1,48 +1,30 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const sequelize = require('./util/database');
 const compression = require('compression');
 const helmet = require('helmet');
 const cors = require('cors');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const userRoutes = require('./routes/user.js');
- 
 
 // Initialize Express app
 const app = express();
 
 // Middleware
-// app.use(helmet());
-
 app.use(
   helmet({
     contentSecurityPolicy: {
       directives: {
         "default-src": ["'self'"],
         "script-src": ["'self'", "https://cdn.jsdelivr.net", "https://cdn.socket.io"],
-        // Add other directives as needed
+        "script-src-attr": ["'self'", "'unsafe-inline'"], // Allow inline event handlers
       },
     },
   })
 );
 
 app.use(compression());
-
 app.use(cors());
-app.options('*', (req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.sendStatus(200); // Respond with HTTP 200 for preflight requests
-});
-
-
-// app.use(cors({
-//   origin: '*', // Replace with your frontend URL
-//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-//   credentials: true,
-// }));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -52,7 +34,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use(userRoutes);
- 
 
 // Database associations
 const User = require('./models/usermodel');
