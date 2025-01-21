eec207633ffecb26a8fe5422d428f0d827740c94
vsbrall143 <vsbrall143@gmail.com>
2025-01-21 18:23:24 +0530

    deploy 13

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sequelize = require('./util/database');
 const compression = require('compression');
-// const helmet = require('helmet');
+const helmet = require('helmet');
 const cors = require('cors');
 const path = require('path');
 
